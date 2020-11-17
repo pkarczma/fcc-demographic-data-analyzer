5f51f4190be61a7a358d57303b64dca805e62fce
pkarczma <przemyslaw.karczmarczyk@cern.ch>
2020-11-17 10:30:30 +0000

    fix: round the values

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -6,13 +6,13 @@ def calculate_demographic_data(print_data=True):
     df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = df['race'].value_counts().tolist()
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = df.loc[df['sex'] == 'Male']['age'].mean()
+    average_age_men = round(df.loc[df['sex'] == 'Male']['age'].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = 100 * df.loc[df['education'] == 'Bachelors'].shape[0] / df.shape[0]
+    percentage_bachelors = round(100 * df.loc[df['education'] == 'Bachelors'].shape[0] / df.shape[0], 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
@@ -23,8 +23,8 @@ def calculate_demographic_data(print_data=True):
     lower_education = df.loc[~education_mask]
 
     # percentage with salary >50K
-    higher_education_rich = 100 * higher_education[higher_education['salary'] == '>50K'].shape[0] / higher_education.shape[0]
-    lower_education_rich = 100 * lower_education[lower_education['salary'] == '>50K'].shape[0] / lower_education.shape[0]
+    higher_education_rich = round(100 * higher_education[higher_education['salary'] == '>50K'].shape[0] / higher_education.shape[0], 1)
+    lower_education_rich = round(100 * lower_education[lower_education['salary'] == '>50K'].shape[0] / lower_education.shape[0], 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = None
