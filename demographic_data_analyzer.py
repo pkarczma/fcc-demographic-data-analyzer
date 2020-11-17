3cc5e785e20197519174ae54f4ca1a919ad76cc0
pkarczma <przemyslaw.karczmarczyk@cern.ch>
2020-11-17 17:29:04 +0000

    feat: percentage of the rich working minimum

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -30,9 +30,11 @@ def calculate_demographic_data(print_data=True):
     min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    hours_mask = (df['hours-per-week'] == min_work_hours)
+    num_min_workers = df.loc[hours_mask].shape[0]
+    num_min_workers_rich = df.loc[hours_mask & (df['salary'] == '>50K')].shape[0]
 
-    rich_percentage = None
+    rich_percentage = 100 * num_min_workers_rich / num_min_workers
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
